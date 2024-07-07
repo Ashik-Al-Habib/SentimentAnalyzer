34519ac44baf16cc3c896ffdc9d79b9c8e99ac1c
Ashik Al Habib <habib62@uwindsor.ca>
2024-07-06 23:33:53 -0400

    - Uploaded all the Updates

--- SentimentAnalyzer/forms.py
+++ SentimentAnalyzer/forms.py
@@ -11,6 +11,7 @@ PRODUCT_IDS = [
     ("B0066AZGD4", "8th Most Reviews - B0066AZGD4"),
     ("B00H5BMH44", "9th Most Reviews - B00H5BMH44"),
     ("B0091JL3OI", "Least Reviews - B0091JL3OI"),
+    ("B00UXLIX00", "Bad Product - B00UXLIX00"),
 ]
 
 ALGORITHMS = [
